4fefbd7b3d413ca3b1cdd06872e4a906737833ac
Yuudai Tamura <y.tamu63@gmail.com>
2025-07-31 23:58:13 +0900

    hands二重定義の削除

--- model/Dealer.java
+++ model/Dealer.java
@@ -5,8 +5,7 @@ public class Dealer extends Player{
     public Dealer(Deck deck){
         super(deck);
     }
-    @SuppressWarnings("unused")
-    private ArrayList<Card> hands = new ArrayList<>();//共有しない手札の宣言
+  
     //手札が17以上であるかを返す処理
     public boolean over_16(){
         return score>16;
